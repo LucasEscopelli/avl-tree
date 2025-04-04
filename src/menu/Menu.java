a0d4d089c26d17daa2ad93d8b2b016dd4bdcfc66
Lucas Escopelli <escopellilucas@gmail.com>
2025-04-04 03:11:46 -0300

    adding some changes

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -10,40 +10,39 @@ import tree.Tree;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.Function;
 
 public class Menu {
-    private final List<Action<Tree<Integer>>> functions = new ArrayList<>();
+    private final List<Action<Tree<Integer>>> actions = new ArrayList<>();
     private final Tree<Integer> tree = new BinaryTree<>();
     private final Scanner scanner = new Scanner(System.in);
 
     public Menu(){
-            functions.add(new CloseApplicationAction<>("0. Fechar aplicacao", scanner));
-            functions.add(new AddAction<>("1. Adicionar Inteiro à árvore.", scanner));
-            functions.add(new GetAction<>("2. Buscar valor na árvore.", scanner));
-            functions.add(new RemoveAction<>("3. Remover valor da árvore.", scanner));
-            functions.add((new PrintAction<>("4. Printar árvore.", scanner)));
+            actions.add(new CloseApplicationAction<>("0. Fechar aplicacao", scanner));
+            actions.add(new AddAction<>("1. Adicionar Inteiro à árvore.", scanner));
+            actions.add(new GetAction<>("2. Buscar valor na árvore.", scanner));
+            actions.add(new RemoveAction<>("3. Remover valor da árvore.", scanner));
+            actions.add((new PrintAction<>("4. Printar árvore.", scanner)));
     }
 
     public void menu() {
         while (true) {
-            printFunctions();
-            Integer choice = UserInteractor.getIntegerValueFromUser(scanner, this::printFunctions);
+            printActions();
+            Integer choice = UserInteractor.getIntegerValueFromUser(scanner, this::printActions);
             ConsoleHandler.clearConsole();
 
             if (isInvalidChoice(choice)) {
                 continue;
             }
 
-            functions.get(choice).accept(tree);
+            actions.get(choice).accept(tree);
         }
     }
 
-    private void printFunctions() {
-        functions.forEach(funcao -> System.out.println(funcao.getName()));
+    private void printActions() {
+        actions.forEach(funcao -> System.out.println(funcao.getName()));
     }
 
     private boolean isInvalidChoice(int choice) {
-        return choice < 0 || choice >= functions.size();
+        return choice < 0 || choice >= actions.size();
     }
 }
