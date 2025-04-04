1ebb11e2fd60df628e2300787ef9a4a0eb159779
Erik Morbach <erik.cruz.morbach@sap.com>
2025-04-03 21:19:01 -0300

    Definindo estrutura do menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import menu.Menu;
+
 public class Main {
     public static void main(String[] args) {
         Integer a = 0;
         Integer b = 1;
         System.out.println(a.compareTo(b));
+        Menu menu = new Menu();
+        do{
+            menu.showMenu();
+            menu.executeOption(menu.getOption());
+        }while(true);
     }
 }
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -10,20 +10,41 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    private final List<Action<Tree<Integer>>> funcoes = new ArrayList<>();
+    private final List<Action<Tree<Integer>>> actions = new ArrayList<>();
     private final Tree<Integer> tree = new BinaryTree<>();
     private Scanner scanner = new Scanner(System.in);
 
     public Menu(){
-            funcoes.add(new AddAction<>("1. Adicionar Inteiro à árvore", scanner));
+            actions.add(new AddAction<>("1. Adicionar Inteiro à árvore", scanner));
     }
 
-    public void menu(){
-        funcoes.forEach(funcao -> {
-            System.out.println(funcao.getName());
-        });
-        Scanner scanner = new Scanner(System.in);
-        int escolha = Integer.parseInt(scanner.nextLine());
-        funcoes.get(escolha + 1).accept(tree);
+    public void showMenu(){
+        System.out.println("Menu:");
+        for(int index = 0;index < this.actions.size();index++){
+            var action = this.actions.get(index);
+            System.out.printf("\t%d. %s\n", index + 1, action);
+        }
+    }
+    public void executeOption(int option){
+        if(!isValidOption(option))
+            throw new RuntimeException("Opção invalida!");
+        option -= 1;
+        this.actions.get(option).accept(this.tree);
+    }
+
+    private boolean isValidOption(int option){
+        return 1 <= option && option <= this.actions.size();
+    }
+
+    public int getOption(){
+        int option = 0;
+        do{
+            System.out.print("Digite a opção desejada: ");
+            option = this.scanner.nextInt();
+            if(!isValidOption(option)){
+                System.out.println("Opção invalida... Digite novamente");
+            }
+        }while(!isValidOption(option));
+        return option;
     }
 }
