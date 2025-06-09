53a87de4fdc7a0f59b4529ee539701e630999b2d
PauloDalsoto <paulodalst@gmail.com>
2025-06-09 15:57:36 -0300

    action LoadFileAction

--- src/main/java/menu/actions/LoadFileAction.java
+++ src/main/java/menu/actions/LoadFileAction.java
@@ -6,15 +6,28 @@ import menu.actions.abstractactions.Action;
 import model.Context;
 
 public class LoadFileAction extends Action<Context>{
-
+	private String defaultPath = "./data/nomes.csv";
 	public LoadFileAction(String name, Scanner scanner) {
 		super(name, scanner);
 	}
 
 	@Override
 	public ActionStatus runAction(Context tree) {
-        System.out.println("Buscando arquivo");
-        return ActionStatus.OK;
+		System.out.print("Digite o caminho do arquivo .CSV (Default: './data/nomes.csv'): ");
+		String path = scanner.nextLine();
+		if (path.isEmpty()) {
+			path = defaultPath;
+		}
+		try {
+			System.out.println("Buscando arquivo...");
+			tree.loadFile(path);
+			System.out.println("Arquivo carregado com sucesso!");
+
+		} catch (Exception e) {
+			System.out.println("Erro ao carregar arquivo: " + e.getMessage());
+			return ActionStatus.FAILED;
+		}
+		return ActionStatus.OK;
 	}
-	
+
 }
