927dda11e7dfc7c3999d75bef6f4e922900c3405
Lucas Escopelli <escopellilucas@gmail.com>
2025-06-12 10:53:19 -0300

    adding logic to comparators

--- src/main/java/menu/actions/GetPersonWithNamePrefixAction.java
+++ src/main/java/menu/actions/GetPersonWithNamePrefixAction.java
@@ -1,9 +1,15 @@
 package menu.actions;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import menu.actions.abstractactions.Action;
+import menu.terminalhandler.UserInteractor;
 import model.Context;
+import model.person.comparators.PersonDateComparator;
+import model.person.comparators.PersonNameComparator;
+import tree.PersonStringUtilsForTree;
 
 public class GetPersonWithNamePrefixAction extends Action<Context> {
 
@@ -13,8 +19,25 @@ public class GetPersonWithNamePrefixAction extends Action<Context> {
 
 	@Override
 	public ActionStatus runAction(Context tree) {
-        System.out.println("Buscando por nomes com determinado prefixo");
-        return ActionStatus.OK;
+		System.out.println("Buscando por primeiro nome");
+		System.out.println("Digite o valor do nome a ser procurado");
+		String prefix = scanner.nextLine();
+
+		System.out.println("Buscando por nome");
+
+		List<PersonNameComparator> personNameComparatorList = PersonStringUtilsForTree.getNamesByPrefix(tree.getNameIndexer(), prefix);
+
+		if(!personNameComparatorList.isEmpty()){
+			personNameComparatorList.forEach(personDateComparator ->
+					System.out.println(personDateComparator.getPerson().toString())
+			);
+
+			return ActionStatus.OK;
+		}
+
+		System.out.println("Pessoas nesse range não existem.");
+
+		return ActionStatus.OK;
 	}
 	
 }
---
+++ src/main/java/tree/PersonStringUtilsForTree.java
@@ -0,0 +1,27 @@
+package tree;
+
+import model.person.comparators.PersonNameComparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonStringUtilsForTree {
+
+    public static List<PersonNameComparator> getNamesByPrefix(Tree<PersonNameComparator> tree, String prefix) {
+        String startName = prefix + " ";
+        String endName = prefix + Character.MAX_VALUE;
+        ArrayList<PersonNameComparator> result = new ArrayList<>();
+
+        PersonNameComparator start = new PersonNameComparator(startName);
+        PersonNameComparator end = new PersonNameComparator(endName);
+
+        final PersonNameComparator person = tree.get(start);
+
+        if(person != null && person.getPerson().getName().equals("prefix")){
+            result.add(person);
+        }
+
+        result.addAll(tree.getBetween(start, end));
+        return result;
+    }
+}
